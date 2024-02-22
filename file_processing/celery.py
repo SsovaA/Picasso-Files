0be3620fe48c0e76772a6016df8ecec6e51d645c
nikolay <stich09@mail.ru>
2024-02-22 15:24:42 +0300

    Rename project

--- file_processing/celery.py
+++ file_processing/celery.py
@@ -1,7 +1,7 @@
 import os
 from celery import Celery
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "picasso_files.settings")
-app = Celery("picasso_files")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "file_processing.settings")
+app = Celery("file_processing")
 app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks()
